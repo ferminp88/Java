8808278506caffbaab614d880095d81f813c6aef
fermin <ferminp88@gmail.com>
2022-08-31 10:29:46 -0300

    sumaYagregarPuerta

--- src/Coche.java
+++ src/Coche.java
@@ -1,10 +1,10 @@
-public  class Coche {
+public  class Coche { // Ejercicio 2
     public int puertas =4;
     int sumarPuertas = puertas;
 
     public void sumarPuertas() {
+
         this.sumarPuertas++;
     }
-
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Coche miCoche = new Coche();
-        miCoche.sumarPuertas();
-        int num1 = 1;
-        int num2 = 2;
-        int num3 = 3;
-        int resultado = suma(num1, num2, num3);
+        int num1 = 1; // Ejercicio 1
+        int num2 = 2; // Ejercicio 1
+        int num3 = 3; // Ejercicio 1
+        int resultado = suma(num1, num2, num3); // Ejercicio 1
 
-        System.out.println("El resultado es "+resultado);
-        System.out.println("La cantidad de puertas del Coche es : " +miCoche.sumarPuertas);
+        Coche miCoche = new Coche(); // Ejercicio 2
+        miCoche.sumarPuertas();// Ejercicio 2
+
+        System.out.println("El resultado es "+resultado); // Ejercicio 1
+        System.out.println("La cantidad de puertas del Coche es : " +miCoche.sumarPuertas);// Ejercicio 2
     }
 
     public static int suma(int num1, int num2, int num3) {
-
-        return num1 + num2 + num3;
-
+        return num1 + num2 + num3;//// Ejercicio 1
     }
 
 }
