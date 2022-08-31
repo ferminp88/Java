f41af8651bedecd90fa0de0e9799223319a34663
fermin <ferminp88@gmail.com>
2022-08-30 23:39:09 -0300

    suma

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int resultado = suma(num1,num2,num3);
+
+        System.out.println("El resultado es "+ resultado);
+    }
+
+    public static int suma(int num1, int num2, int num3) {
+
+        return num1 + num2 + num3;
+
+    }
+
+}
